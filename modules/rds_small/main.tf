d2f8804b0a5a2f372eff653c1a06b624b8f40dc2
rhill67 <roger.hill4467@outlook.com>
2026-02-20 19:35:27 -0600

    Changed RDS module port from 3306 to 5432 for postgresql

--- modules/rds_small/main.tf
+++ modules/rds_small/main.tf
@@ -8,8 +8,8 @@ resource "aws_security_group" "rds" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = [data.aws_vpc.this.cidr_block]
   }
